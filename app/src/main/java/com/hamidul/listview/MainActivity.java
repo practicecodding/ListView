e81d6f94f8ef116867f94e15dc42a1f8ca839478
user <practicecoddin@gmail.com>
2023-09-28 06:59:36 +0600

    ListView

--- app/src/main/java/com/hamidul/listview/MainActivity.java
+++ app/src/main/java/com/hamidul/listview/MainActivity.java
@@ -3,12 +3,49 @@ package com.hamidul.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        MyAdapter myAdapter = new MyAdapter();
+
+        listView.setAdapter(myAdapter);
+
+
+
     }
+
+    private class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
+
 }
